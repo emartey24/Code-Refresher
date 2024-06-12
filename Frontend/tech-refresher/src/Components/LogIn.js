1dcfee00f1b58343518c74dd1160a54f99e87d74
COR Coding <ls>
2024-06-12 15:43:13 -0400

    Added Login Box(In-Progress)

--- Frontend/tech-refresher/src/Components/LogIn.js
+++ Frontend/tech-refresher/src/Components/LogIn.js
@@ -1,30 +1,38 @@
 import React from 'react'
 import '../LogIn.css';
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+
+
 
 const LogIn = ()=> {
   return (
-    <div class="wrapper">
-      <form action="">
-        <h1>Login</h1>
-        <div className='input-box'>
-          <input type="text" placeholder='Username' required></input>
-        </div>
-        <div className='input-box'>
-          <input type="password" placeholder='Password' required></input>
-        </div>
-        
-        <div className="remember-forgot">
-          <label><input type="checkout" />Remember me</label>
-          <a href='#'>Forgot Password</a>
-        </div>
+    <div class="loginBody">
+            <form className="wrapper"action="">
+              <h1>Login</h1>
+              <div className='input-box'>
+                <input type="text" placeholder='Username here' required></input>
+                <FaUser className='icon' />
+              </div>
+              <div className='input-box'>
+                <input type="password" placeholder='Password here' required></input>
+                <FaLock className='icon'/>
+              </div>
+              
+              {/* <div className="remember-forgot">
+                <label><input type="checkout" />Remember me</label>
+                <a href='#'>Forgot Password</a>
+              </div> */}
 
 
-<button type="submit">Login</button>
+               <button type="submit">Login</button>
 
-<div className='register-link'>
-  <p>Don't have an account? <a href="#">Register</a></p>
-</div>
-      </form>
+                  <div className='register-link'>
+                    <p>Don't have an account? <a href="#">Register</a></p>
+                  </div>
+
+
+                   </form>
 
 
     </div>
