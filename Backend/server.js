e8e031e95c500398f22f905fa7430951ddfb7c38
COR Coding <ls>
2024-06-18 17:00:34 -0400

    Server is updated

--- Backend/server.js
+++ Backend/server.js
@@ -124,7 +124,7 @@ app.delete('/tech/:id', async (req, res) => {
 
 
 
-app.listen(3001, () => {
-  console.log('Server is running on port 3001');
+app.listen(3003, () => {
+  console.log('Server is running on port 3003');
 });
 
