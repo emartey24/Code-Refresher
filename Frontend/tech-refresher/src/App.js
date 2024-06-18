6a25ba72dfb4aff508ff5715e60ec9e5e89c12eb
COR Coding <ls>
2024-06-18 09:53:42 -0400

    Added register page and fixed login

--- Frontend/tech-refresher/src/App.js
+++ Frontend/tech-refresher/src/App.js
@@ -1,41 +1,43 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Navbarcomponent from "./Components/Navbarcomponent";
-import HowtoPlay from "./Components/HowtoPlay";
-import Home from "./Components/Home";
-import LogIn from "./Components/LogIn";
-import Donate from "./Components/Donate";
-import Game from "./Components/Game";
-
+import Navbarcomponent from './Components/Navbarcomponent';
+import HowtoPlay from './Components/HowtoPlay';
+import Home from './Components/Home';
+import LogIn from './Components/LogIn';
+import Donate from './Components/Donate';
+import Game from './Components/Game';
+import RegisterPage from './Components/RegisterPage';
 
 function App() {
+  const handleLogin = () => {
+    // Perform any additional login logic here if needed
+    // Navigate to the game page after login
+    console.log('Login successful');
+    // Replace with your navigation logic after login
+    // For example:
+    // navigate('/game');
+  };
 
-
-
-
-  
   return (
-    <>
-      
-
-   
-     <div>
-        <BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Navbarcomponent />
         <Routes>
-          <Route path="/" element={<Navbarcomponent/>}>
-            <Route index element={<Home/>} />
-            <Route path="howtoplay" element={<HowtoPlay />} /> 
-            <Route path="login" element={<LogIn />} /> 
-            <Route path="Shop" element={<Donate />} />
-            <Route path="Game" element={<Game />} />
-          </Route>
+          <Route path="/" element={<RegisterPage />} />
+          <Route path="home" element={<Home />} />
+          <Route path="howtoplay" element={<HowtoPlay />} />
+          <Route
+            path="login"
+            element={<LogIn onLogin={handleLogin} />}
+          />
+          <Route path="shop" element={<Donate />} />
+          <Route path="game" element={<Game />} />
         </Routes>
       </BrowserRouter>
-      </div>
-
-      </>
-
+    </div>
   );
 }
 
 export default App;
+
--- Frontend/tech-refresher/src/Components/Navbarcomponent.js
+++ Frontend/tech-refresher/src/Components/Navbarcomponent.js
@@ -9,7 +9,8 @@ const Navbarcomponent = () => {
     return (
       <>
         <ul >
-           <li><a><Link to="/">Home</Link></a></li> 
+           <li><a><Link to="/">Register here</Link></a></li> 
+           <li><a><Link to="/home">Home Page</Link></a></li> 
            <li><a><Link to="/howtoplay">How to Play</Link></a></li> 
            <li><a><Link to="/login">Log In</Link></a></li> 
            <li><a><Link to="/Shop">Shop</Link></a></li> 
