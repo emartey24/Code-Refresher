f826718849083a46a1948d2a547202e2a081e0fc
COR Coding <corcoding@DESKTOP-KR7OU36.localdomain>
2024-06-19 12:26:13 -0400

    fixing deploy issues

--- Frontend/tech-refresher/src/Components/Navbarcomponent.js
+++ Frontend/tech-refresher/src/Components/Navbarcomponent.js
@@ -1,21 +1,18 @@
-import React from 'react'
-import { Outlet, Link } from "react-router-dom";
-
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
 
 const Navbarcomponent = () => {
-    return (
-      <>
-        <ul >
-           {/* <li><a><Link to="/">Register here</Link></a></li>  */}
-           <li><a><Link to="/home">Home Page</Link></a></li> 
-           <li><a><Link to="/studyguide">Self Study Guide</Link></a></li> 
-           {/* <li><a><Link to="/login">Log In</Link></a></li>  */}
-           <li><a><Link to="/flashcards">Flashcards</Link></a></li> 
-           <li><a><Link to="/Shop">Shop</Link></a></li> 
-        </ul>
-        <Outlet />
-      </>
-    )
-  };
-  
-  export default Navbarcomponent;
+  return (
+    <>
+      <ul>
+        <li><Link to="/home">Home Page</Link></li>
+        <li><Link to="/studyguide">Self Study Guide</Link></li>
+        <li><Link to="/flashcards">Flashcards</Link></li>
+        <li><Link to="/shop">Shop</Link></li>
+      </ul>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navbarcomponent;
